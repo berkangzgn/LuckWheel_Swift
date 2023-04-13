00a2c3546c765e1857ea98b603d141e3a98e00ab
berkangzgn <berkangzgn@protonmail.com>
2023-04-13 17:28:20 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LuckWheel_Swift
+
+A description of this package.
---
+++ Tests/LuckWheel_SwiftTests/LuckWheel_SwiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LuckWheel_Swift
+
+final class LuckWheel_SwiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LuckWheel_Swift().text, "Hello, World!")
+    }
+}
