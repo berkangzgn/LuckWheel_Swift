2820fcb3ccdde50c7ab192c23a18d66b223d61e8
berkangzgn <berkangzgn@protonmail.com>
2023-04-15 01:43:20 +0300

    Images added

--- Sources/LuckWheel_Swift/LuckWheel_Swift.swift
+++ Sources/LuckWheel_Swift/LuckWheel_Swift.swift
@@ -1,6 +1,6 @@
 public struct LuckWheel_Swift {
     
-    let luckWheelVC: LuckWheelVC!
+    let luckWheelVC: LuckWheelVC?
     
     public init() {
         luckWheelVC =  LuckWheelVC()
@@ -8,7 +8,7 @@ public struct LuckWheel_Swift {
     
     public func turnWheel(title1: String, title2: String, title3: String, title4: String, title5: String, title6: String, title7: String, title8: String) {
         
-        luckWheelVC.segmentText = [title1, title2, title3, title4, title5, title6, title7, title8]
-        luckWheelVC.playScene?.startRotation()
+        luckWheelVC!.segmentText = [title1, title2, title3, title4, title5, title6, title7, title8]
+        luckWheelVC!.playScene?.startRotation()
     }
 }
